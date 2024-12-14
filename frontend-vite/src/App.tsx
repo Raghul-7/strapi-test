3a891d1e62e48b2169090337b98a3e2a9e6bc165
Raghul-7 <r.raghulkrishna@resulticksmail.com>
2024-12-14 16:50:13 +0530

    add: vite and strapi

---
+++ frontend-vite/src/App.tsx
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <header>
+     <p className="text-red-500">Hello vite</p> 
+      <nav className="">
+        {/* logo */}
+        <img src="" alt="" />
+        {/* links */}
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">about</a>
+          </li>
+          <li>
+            <a href="">services</a>
+          </li>
+          <li>
+            <a href="">contact</a>
+          </li>
+        </ul>
+        {/* btn */}
+        <button>explore more</button>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
